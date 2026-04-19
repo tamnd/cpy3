c2c052cce13bec90d2e90385a0508193e50897c5
Duc-Tam Nguyen <tamnd@liteio.dev>
2026-04-19 13:24:07 +0700

    python-3.14: adapt build and remove APIs deleted in 3.13/3.14
    Switch pkg-config from python3 to python-3.14-embed. Drop the
    Py_Set* family (Py_SetProgramName, Py_SetPath, Py_SetPythonHome,
    Py_SetStandardStreamEncoding, PySys_SetPath, PySys_SetArgv,
    PySys_SetArgvEx, PySys_AddWarnOption, PySys_ResetWarnOptions,
    PySys_AddXOption) and the free-list clearers
    (PyDict_ClearFreeList, PyFloat_ClearFreeList, PyList_ClearFreeList).
    Remove PyEval_InitThreads, PyEval_ThreadsInitialized and
    PyEval_ReInitThreads. Add Py_IsFinalizing, made public in 3.13.
    
    Introduce PyConfig as the replacement surface: NewPyConfig with
    PyConfigPython / PyConfigIsolated modes, SetProgramName,
    SetPythonHome, SetStdioEncoding, SetArgv, SetModuleSearchPaths,
    plus PyStatus for return-value plumbing.

--- lifecycle.go
+++ lifecycle.go
@@ -16,14 +16,6 @@ import (
 	"unsafe"
 )
 
-// The argument for Py_SetProgramName, Py_SetPath and Py_SetPythonHome should point to a zero-terminated wide character string in static storage
-// whose contents will not change for the duration of the program’s execution
-var (
-	programName *C.wchar_t
-	pythonPath  *C.wchar_t
-	pythonHome  *C.wchar_t
-)
-
 //Py_Initialize : https://docs.python.org/3/c-api/init.html#c.Py_Initialize
 func Py_Initialize() {
 	C.Py_Initialize()
@@ -43,6 +35,13 @@ func Py_IsInitialized() bool {
 	return C.Py_IsInitialized() != 0
 }
 
+//Py_IsFinalizing : https://docs.python.org/3/c-api/init.html#c.Py_IsFinalizing
+//
+// Public since Python 3.13 (previously _Py_IsFinalizing).
+func Py_IsFinalizing() bool {
+	return C.Py_IsFinalizing() != 0
+}
+
 //Py_FinalizeEx : https://docs.python.org/3/c-api/init.html#c.Py_FinalizeEx
 func Py_FinalizeEx() int {
 	return int(C.Py_FinalizeEx())
@@ -53,35 +52,43 @@ func Py_Finalize() {
 	C.Py_Finalize()
 }
 
-//Py_SetStandardStreamEncoding : https://docs.python.org/3/c-api/init.html#c.Py_SetStandardStreamEncoding
-func Py_SetStandardStreamEncoding(encoding, errors string) int {
-	cencoding := C.CString(encoding)
-	defer C.free(unsafe.Pointer(cencoding))
-
-	cerrors := C.CString(errors)
-	defer C.free(unsafe.Pointer(cerrors))
-
-	return int(C.Py_SetStandardStreamEncoding(cencoding, cerrors))
-
+//Py_GetVersion : https://docs.python.org/3/c-api/init.html#c.Py_GetVersion
+func Py_GetVersion() string {
+	cversion := C.Py_GetVersion()
+	return C.GoString(cversion)
 }
 
-//Py_SetProgramName : https://docs.python.org/3/c-api/init.html#c.Py_SetProgramName
-func Py_SetProgramName(name string) error {
-	cname := C.CString(name)
-	defer C.free(unsafe.Pointer(cname))
-
-	newProgramName := C.Py_DecodeLocale(cname, nil)
-	if newProgramName == nil {
-		return fmt.Errorf("fail to call Py_DecodeLocale on '%s'", name)
-	}
-	C.Py_SetProgramName(newProgramName)
-
-	//no operation is performed if nil
-	C.PyMem_RawFree(unsafe.Pointer(programName))
-	programName = newProgramName
-
-	return nil
+//Py_GetPlatform : https://docs.python.org/3/c-api/init.html#c.Py_GetPlatform
+func Py_GetPlatform() string {
+	cplatform := C.Py_GetPlatform()
+	return C.GoString(cplatform)
 }
+
+//Py_GetCopyright : https://docs.python.org/3/c-api/init.html#c.Py_GetCopyright
+func Py_GetCopyright() string {
+	ccopyright := C.Py_GetCopyright()
+	return C.GoString(ccopyright)
+}
+
+//Py_GetCompiler : https://docs.python.org/3/c-api/init.html#c.Py_GetCompiler
+func Py_GetCompiler() string {
+	ccompiler := C.Py_GetCompiler()
+	return C.GoString(ccompiler)
+}
+
+//Py_GetBuildInfo : https://docs.python.org/3/c-api/init.html#c.Py_GetBuildInfo
+func Py_GetBuildInfo() string {
+	cbuildInfo := C.Py_GetBuildInfo()
+	return C.GoString(cbuildInfo)
+}
+
+// Py_GetProgramName, Py_GetPythonHome, Py_GetPath, Py_GetPrefix,
+// Py_GetExecPrefix and Py_GetProgramFullPath are soft-deprecated in 3.13
+// and are going away in 3.15. We wrap them so callers who only need to
+// read configuration after Py_Initialize keep working during the 3.14
+// cycle. Use PyConfig for new code.
+//
+// PyConfig is defined in config.go.
 
 //Py_GetProgramName : https://docs.python.org/3/c-api/init.html#c.Py_GetProgramName
 func Py_GetProgramName() (string, error) {
@@ -156,119 +163,6 @@ func Py_GetPath() (string, error) {
 	defer C.PyMem_Free(unsafe.Pointer(cname))
 
 	return C.GoString(cname), nil
-}
-
-//Py_SetPath : https://docs.python.org/3/c-api/init.html#c.Py_SetPath
-func Py_SetPath(path string) error {
-	cpath := C.CString(path)
-	defer C.free(unsafe.Pointer(cpath))
-
-	newPath := C.Py_DecodeLocale(cpath, nil)
-	if newPath == nil {
-		return fmt.Errorf("fail to call Py_DecodeLocale on '%s'", path)
-	}
-	C.Py_SetPath(newPath)
-
-	C.PyMem_RawFree(unsafe.Pointer(pythonPath))
-	pythonHome = newPath
-
-	return nil
-}
-
-//Py_GetVersion : https://docs.python.org/3/c-api/init.html#c.Py_GetVersion
-func Py_GetVersion() string {
-	cversion := C.Py_GetVersion()
-	return C.GoString(cversion)
-}
-
-//Py_GetPlatform : https://docs.python.org/3/c-api/init.html#c.Py_GetPlatform
-func Py_GetPlatform() string {
-	cplatform := C.Py_GetPlatform()
-	return C.GoString(cplatform)
-}
-
-//Py_GetCopyright : https://docs.python.org/3/c-api/init.html#c.Py_GetCopyright
-func Py_GetCopyright() string {
-	ccopyright := C.Py_GetCopyright()
-	return C.GoString(ccopyright)
-}
-
-//Py_GetCompiler : https://docs.python.org/3/c-api/init.html#c.Py_GetCompiler
-func Py_GetCompiler() string {
-	ccompiler := C.Py_GetCompiler()
-	return C.GoString(ccompiler)
-}
-
-//Py_GetBuildInfo : https://docs.python.org/3/c-api/init.html#c.Py_GetBuildInfo
-func Py_GetBuildInfo() string {
-	cbuildInfo := C.Py_GetBuildInfo()
-	return C.GoString(cbuildInfo)
-}
-
-//PySys_SetArgvEx : https://docs.python.org/3/c-api/init.html#c.PySys_SetArgvEx
-func PySys_SetArgvEx(args []string, updatepath bool) error {
-	argc := C.int(len(args))
-	argv := make([]*C.wchar_t, argc, argc)
-	for i, arg := range args {
-		carg := C.CString(arg)
-		defer C.free(unsafe.Pointer(carg))
-
-		warg := C.Py_DecodeLocale(carg, nil)
-		if warg == nil {
-			return fmt.Errorf("fail to call Py_DecodeLocale on '%s'", arg)
-		}
-		// Py_DecodeLocale requires a call to PyMem_RawFree to free the memory
-		defer C.PyMem_RawFree(unsafe.Pointer(warg))
-
-		argv[i] = warg
-	}
-
-	if updatepath {
-		C.PySys_SetArgvEx(argc, (**C.wchar_t)(unsafe.Pointer(&argv[0])), 1)
-	} else {
-		C.PySys_SetArgvEx(argc, (**C.wchar_t)(unsafe.Pointer(&argv[0])), 0)
-	}
-
-	return nil
-}
-
-//PySys_SetArgv : https://docs.python.org/3/c-api/init.html#c.PySys_SetArgv
-func PySys_SetArgv(args []string) error {
-	argc := C.int(len(args))
-	argv := make([]*C.wchar_t, argc, argc)
-	for i, arg := range args {
-		carg := C.CString(arg)
-		defer C.free(unsafe.Pointer(carg))
-
-		warg := C.Py_DecodeLocale(carg, nil)
-		if warg == nil {
-			return fmt.Errorf("fail to call Py_DecodeLocale on '%s'", arg)
-		}
-		// Py_DecodeLocale requires a call to PyMem_RawFree to free the memory
-		defer C.PyMem_RawFree(unsafe.Pointer(warg))
-
-		argv[i] = warg
-	}
-	C.PySys_SetArgv(argc, (**C.wchar_t)(unsafe.Pointer(&argv[0])))
-
-	return nil
-}
-
-//Py_SetPythonHome : https://docs.python.org/3/c-api/init.html#c.Py_SetPythonHome
-func Py_SetPythonHome(home string) error {
-	chome := C.CString(home)
-	defer C.free(unsafe.Pointer(chome))
-
-	newHome := C.Py_DecodeLocale(chome, nil)
-	if newHome == nil {
-		return fmt.Errorf("fail to call Py_DecodeLocale on '%s'", home)
-	}
-	C.Py_SetPythonHome(newHome)
-
-	C.PyMem_RawFree(unsafe.Pointer(pythonHome))
-	pythonHome = newHome
-
-	return nil
 }
 
 //Py_GetPythonHome : https://docs.python.org/3/c-api/init.html#c.Py_GetPythonHome
